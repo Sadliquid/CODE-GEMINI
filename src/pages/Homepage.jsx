4aa48382ded4ac3fa5c79d67a4785b9ea77e4ad4
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-21 22:59:41 +0800

    Added animated background

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,27 +4,35 @@ import MainCard from '../components/MainCard';
 function Homepage() {
 
 	return (
-		<Box
-            position="relative"
-            bgImage="url('src/assets/Ocean.png')" 
-            bgSize="cover"
-            bgPosition="center"
-            bgAttachment="fixed" 
-            minHeight="100vh"
-            width="100%" 
-            overflow="hidden" 
-        >
-			<Box
-				position="absolute"
-				top={0}
-				left={0}
-				right={0}
-				bottom={0}
-				bg="rgba(0, 0, 0, 0.3)"
-			>
-				<MainCard />
+		<>
+			<Box position="relative" minHeight="100vh" width="100%" overflow="hidden">
+				<Box
+					as="video"
+					src="src/assets/lofiBackground.mp4"
+					autoPlay
+					loop
+					muted
+					playsInline
+					objectFit="cover"
+					position="absolute"
+					top={0}
+					left={0}
+					width="100%"
+					height="100%"
+					zIndex={-1}
+				/>
+				<Box
+					position="absolute"
+					top={0}
+					left={0}
+					right={0}
+					bottom={0}
+					bg="rgba(0, 0, 0, 0.3)"
+				>
+					<MainCard />
+				</Box>
 			</Box>
-		</Box>
+		</>
 	);
 }
 
