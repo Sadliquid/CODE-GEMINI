dc1345e7a05019c3a61b41e6347b8f06e54fa936
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-22 21:41:54 +0800

    Added click to expand feature

--- src/components/ImagesSection.jsx
+++ src/components/ImagesSection.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import { useState, useEffect } from 'react';
-import { Card, Box, Button, useToast } from "@chakra-ui/react";
+import { Card, Box, Button, useToast, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { FaExpand } from 'react-icons/fa';
 import ImageCard from "./ImageCard";
 
 function ImagesSection() {
     const [scrollPosition, setScrollPosition] = useState(0);
     const [autoScroll, setAutoScroll] = useState(true);
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const toast = useToast();
 
     const images = [
@@ -64,82 +68,125 @@ function ImagesSection() {
     }, [autoScroll, images.length]);
 
     return (
-        <Card
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            width="100%"
-            height="100%"
-            overflow="hidden"
-            position="relative"
-            borderRadius="2xl"
-            border="2px solid white"
-        
-        >
-            <Box 
+        <>
+            <Card
                 display="flex"
+                justifyContent="center"
                 alignItems="center"
                 width="100%"
                 height="100%"
+                overflow="hidden"
+                position="relative"
+                borderRadius="2xl"
+                border="2px solid white"
             >
                 <Box 
                     display="flex"
-                    overflow="hidden"
+                    alignItems="center"
                     width="100%"
                     height="100%"
                 >
-                    <Box
+                    <Box 
                         display="flex"
-                        flexDirection="row"
-                        transform={`translateX(-${scrollPosition * 100}%)`}
-                        transition="transform 1s ease-in-out"
-                        cursor={autoScroll ? "default" : "pointer"}
-                        width={`${images.length * 100}%`}
+                        overflow="hidden"
+                        width="100%"
                         height="100%"
-                        onClick={enableAutoScroll}
                     >
-                        {images.map((src, index) => (
-                            <Box
-                                key={index}
-                                flexShrink={0}
-                                width="100%"
-                                height="100%"
-                            >
-                                <ImageCard src={src} />
-                            </Box>
-                        ))}
+                        <Box
+                            display="flex"
+                            flexDirection="row"
+                            transform={`translateX(-${scrollPosition * 100}%)`}
+                            transition="transform 1s ease-in-out"
+                            cursor={autoScroll ? "default" : "pointer"}
+                            width={`${images.length * 100}%`}
+                            height="100%"
+                            onClick={enableAutoScroll}
+                        >
+                            {images.map((src, index) => (
+                                <Box
+                                    key={index}
+                                    flexShrink={0}
+                                    width="100%"
+                                    height="100%"
+                                >
+                                    <ImageCard src={src} />
+                                </Box>
+                            ))}
+                        </Box>
+                    </Box>
+                    <Button
+                        onClick={handleScrollLeft}
+                        isDisabled={scrollPosition === 0}
+                        position="absolute"
+                        left="0"
+                        top="50%"
+                        transform="translateY(-50%)"
+                        borderRadius="full"
+                        boxShadow="md"
+                        p={2}
+                        ml={2}
+                        zIndex="docked"
+                    >
+                        <ChevronLeftIcon boxSize={6} />
+                    </Button>
+                    <Button
+                        onClick={handleScrollRight}
+                        isDisabled={scrollPosition === images.length - 1}
+                        position="absolute"
+                        right="0"
+                        top="50%"
+                        transform="translateY(-50%)"
+                        borderRadius="full"
+                        boxShadow="md"
+                        p={2}
+                        mr={2}
+                        zIndex="docked"
+                    >
+                        <ChevronRightIcon boxSize={6} />
+                    </Button>
+                    <Box
+                        position="absolute"
+                        top="2"
+                        right="2"
+                        borderRadius="lg"
+                        backgroundColor="rgba(255, 255, 255, 0.2)"
+                        boxShadow="md"
+                        zIndex="docked"
+                        p={2}
+                        onClick={() => {
+                            setAutoScroll(false);
+                            onOpen();
+                        }}
+                        sx={{ 
+                            cursor: "pointer",
+                            transition: "0.2s ease-in-out",
+                            _hover: {
+                                backgroundColor: "rgba(255, 255, 255, 0.3)",
+                                transition: "0.2s ease-in-out"
+                            }
+                        }}
+                    >
+                        <FaExpand size={20} color="white" />
                     </Box>
                 </Box>
-                <Button
-                    onClick={handleScrollLeft}
-                    isDisabled={scrollPosition === 0}
-                    position="absolute"
-                    left="0"
-                    top="50%"
-                    transform="translateY(-50%)"
-                    borderRadius="full"
-                    boxShadow="md"
-                    p={2}
-                    ml={2}
-                >
-                    <ChevronLeftIcon boxSize={6} />
-                </Button>
-                <Button
-                    onClick={handleScrollRight}
-                    isDisabled={scrollPosition === images.length - 1}
-                    position="absolute"
-                    right="0"
-                    top="50%"
-                    transform="translateY(-50%)"
-                    borderRadius="full"
-                    boxShadow="md"
-                    p={2}
-                    mr={2}
-                >
-                    <ChevronRightIcon boxSize={6} />
-                </Button>
-            </Box>
-        </Card>
+            </Card>
+            {/* modal with just the selected expanded image here */}
+            <Modal isOpen={isOpen} onClose={onClose} size="full">
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader />
+                    <ModalCloseButton
+                        onClick={() => {
+                            setAutoScroll(true);
+                            onClose();
+                        }}
+                    />
+                    <ModalBody mt={2}>
+                        <ImageCard src={images[scrollPosition]} />
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
 
